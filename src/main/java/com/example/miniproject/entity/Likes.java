563979d7234cf77b0d6ee08aac929e1fcae3960c
oneqrhee <blaackpurin@gmail.com>
2022-08-12 14:59:52 +0900

    초기 entity 추가

---
+++ src/main/java/com/example/miniproject/entity/Likes.java
@@ -0,0 +1,27 @@
+package com.example.miniproject.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Post post;
+}
