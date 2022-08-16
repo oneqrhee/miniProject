f40d1570a86d00df1b0751192f7ee1259e4d6f90
oneqrhee <blaackpurin@gmail.com>
2022-08-17 01:21:59 +0900

    CORS 충돌 해결

--- src/main/java/com/example/miniproject/security/SecurityConfig.java
+++ src/main/java/com/example/miniproject/security/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.example.miniproject.security;
 
 import com.example.miniproject.repository.MemberRepository;
 import com.example.miniproject.security.config.jwt.JwtAuthorizationFilter;
+import io.netty.handler.codec.http.cors.CorsConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -14,16 +17,21 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
     @Autowired
     private MemberRepository memberRepository;
 
-    @Autowired
-    private CorsConfig config;
+//    @Autowired
+//    private CorsConfig config;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -45,12 +53,11 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .csrf().disable() //Csrf 토큰요청 기능 OFF( JWT나 OAuth2 사용시 불필요)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//RestApi 이므로 Statless, 세션 사용안함.
-        http
-                .authorizeRequests()
-                .antMatchers("/api/member/**", "/api/products/**").permitAll()
-                .anyRequest().authenticated();
-
-
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/api/member/**", "/api/products/**").permitAll()
+//                .anyRequest().authenticated();
+        http.cors();
 
         return http.build();
     }
@@ -60,17 +67,29 @@ public class SecurityConfig {
         public void configure(HttpSecurity http) throws Exception {
             AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
             http
-                    .addFilter(config.corsFilter())
+//                    .addFilter(config.corsFilter())
                     .addFilter(new JwtAuthorizationFilter(authenticationManager));
-
-//                    .authorizeRequests()
-//                    .antMatchers("/api/member/**", "/api/products/**").permitAll()
-//                    .anyRequest().authenticated();
-
-
+            http
+                    .authorizeRequests()
+                    .antMatchers("/api/member/**", "/api/products/**").permitAll()
+                    .anyRequest().authenticated();
         }
+    }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        configuration.addExposedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        configuration.addAllowedOriginPattern("*"); // 모든 IP에 대한 응답을 허용
+        return source;
     }
 }
 
